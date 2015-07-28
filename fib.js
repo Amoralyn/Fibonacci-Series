2eab6f4e86ba8b226ea483d2357425d3a6dbc1df
Lovelyn Tijesunimi-Israel <lovelynisrael@hotmail.com>
2015-07-28 16:50:48 +0100

    Add JS and HTML files

---
+++ fib.js
@@ -0,0 +1,40 @@
+// var fib = [];
+// fib[0]=0;
+// fib[1]=1;
+// for(var i=2; i<=100; i++){
+//     fib[i]=fib[i-2]+fib[i-1];
+//     console.log(fib[i]);
+// }
+
+// var fib = function(i){
+//     if(i < 2){
+//         return 1;
+//     }else{
+//         return fib(i-2) + fib(i-1);
+//     }
+// };
+// fib(100);
+
+
+// var prime = function(num){
+//     if(num/num == 1 && num/1 == num){
+//         return (num + 'is a prime number');
+//     }
+// };
+
+
+// var prime = function(num){
+// var primeArr = [];
+// prime[0] = 1;
+// prime[1] = 2;
+// for (var i= 2; i<=num; i++){
+//     if(num%2 === 0 ||num%3 === 0 || num%5 === 0){
+//         return (num + 'is not a prime number');
+//     }else {
+//         return (num + 'is a prime number');
+//     }
+// }
+// };
+
+// prime(100);
+
